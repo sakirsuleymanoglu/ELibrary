a3efedf052f190f918260e29d6d3af4ef0334c5a
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2022-11-29 21:26:13 +0300

    add updaterange

--- src/Core/ELibrary.Application/Abstractions/EntityServices/IEntityUpdater.cs
+++ src/Core/ELibrary.Application/Abstractions/EntityServices/IEntityUpdater.cs
@@ -3,5 +3,6 @@
     public interface IEntityUpdater
     {
         void Update<T>(T entity) where T : class;
+        void UpdateRange<T>(params T[] entities) where T : class;
     }
 }
--- src/Infrastructure/ELibrary.Persistence/EntityServices/EntityFramework/EfEntityUpdater.cs
+++ src/Infrastructure/ELibrary.Persistence/EntityServices/EntityFramework/EfEntityUpdater.cs
@@ -8,5 +8,6 @@ namespace ELibrary.Persistence.EntityServices.EntityFramework
         readonly DbContext _context;
         public EfEntityUpdater(DbContext context) => _context = context;
         public void Update<T>(T entity) where T : class => _context.Set<T>().Update(entity);
+        public void UpdateRange<T>(params T[] entities) where T : class => _context.Set<T>().UpdateRange(entities);
     }
 }
